07116b5fef9c0bc37972f2e80a43cd3f22190997
akira-charon <akira@charondevelopment.com>
2017-10-20 16:51:05 -0600

    placed form items, added validations.

---
+++ src/components/Home.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="home">
+
+    <Card :bordered="false">
+      <p slot="title">ProLease SAML Service Provider File Generator</p>
+      <Form ref="formValidate" :model="formValidate" :rules="ruleValidate" :label-width="120">
+        <FormItem label="Account Directory" prop="accountDirectory">
+          <Input v-model="formValidate.accountDirectory" placeholder="PL_XYZCompany" />
+        </FormItem>
+        <FormItem label="Expiration Date" prop="expirationDate">
+          <Row type="flex" justify="start">
+            <DatePicker v-model="formValidate.expirationDate" type="date" placeholder="2020-12-31"></DatePicker>
+          </Row>
+        </FormItem>
+        <FormItem label="Logout URL" prop="logoutUrl">
+          <Input v-model="formValidate.logoutUrl" placeholder="http://www.mycompany.com/saml/logout.html" />
+        </FormItem>
+        <FormItem label="X509 Certificate" prop="certificate">
+          <Input v-model="formValidate.certificate" type="textarea" :rows="20" />
+        </FormItem>
+        <FormItem>
+          <Button type="primary" @click="handleSubmit('formValidate')">Generate SAML</Button>
+          <Button type="ghost" @click="handleReset('formValidate')" style="margin-left: 8px">Clear All</Button>
+        </FormItem>
+      </Form>
+    </Card>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'home',
+    data() {
+      return {
+        formValidate: {
+          accountDirectory: '',
+          expirationDate: '',
+          logoutUrl: '',
+          certificate: '',
+        },
+        ruleValidate: {
+          accountDirectory: [
+            { required: true, message: 'Account Directory cannot be blank', trigger: 'blur' },
+          ],
+          expirationDate: [
+            { required: true, type: 'date', message: 'Expiration Date cannot be blank', trigger: 'change' },
+          ],
+          certificate: [
+            { required: true, message: 'X509 Certificate cannot be blank', trigger: 'blur' },
+          ],
+        },
+      };
+    },
+    methods: {
+      handleSubmit(name) {
+        this.$refs[name].validate((valid) => {
+          if (valid) {
+            this.$Message.success('Generated SAML Service Provider file!');
+          } else {
+            this.$Message.error('Please make sure to fill all required fields!');
+          }
+        });
+      },
+      handleReset(name) {
+        this.$refs[name].resetFields();
+      },
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  #home{
+    width: 700px;
+  }
+
+</style>
